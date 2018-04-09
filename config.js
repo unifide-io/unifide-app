78a1f4f16164dba4c5a4c1977ba367ce08983395
mcmillennick <mcmillennick@gmail.com>
2018-04-09 02:02:12 -0700

    polymer3 improvements and app-layout foundation
    //updated more dependencies to polymer3 standards
    //implemented the app-layout and app-drawer

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
-    apiKey: "AIzaSyD5mF0DS7W3W6kncGbYJ_YTpih9jvla6QE",
-    authDomain: "unifide-beta.firebaseapp.com",
-    databaseURL: "https://unifide-beta.firebaseio.com",
-    projectId: "unifide-beta",
-    storageBucket: "unifide-beta.appspot.com",
-    messagingSenderId: "143206016593"
+    apiKey: "AIzaSyDL6r1KJKUnkwQplmRx6oeXq96SPFLwav8",
+    authDomain: "unifide-app.firebaseapp.com",
+    databaseURL: "https://unifide-app.firebaseio.com",
+    projectId: "unifide-app",
+    storageBucket: "unifide-app.appspot.com",
+    messagingSenderId: "505934365223"
   };
